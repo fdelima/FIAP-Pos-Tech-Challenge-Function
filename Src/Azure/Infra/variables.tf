e1eac34b8f6f413d338dc649d665aba608c3bdec
Fernando Lima <fernando@umavida.com>
2024-09-20 18:07:01 -0300

    Implementação terraform e reestruturação

---
+++ Src/Azure/Infra/variables.tf
@@ -0,0 +1,23 @@
+variable "resource_group_name" {
+  description = "Name of the Azure Resource Group"
+  type        = string
+  default     = "fiap-pos-tech-teste"
+}
+
+variable "storage_account_name" {
+  description = "Name of the Azure Storage Account"
+  type        = string
+  default     = "tokenvalidationfuncsa"
+}
+
+variable "service_plan_name" {
+  description = "Name of the Azure Service Plan"
+  type        = string
+  default     = "token-validation-function-app-plan"
+}
+
+variable "azurerm_linux_function_app"{
+    description = "Name of the Azure Function App"
+    type        = string
+    default     = "token-validation-function-app"
+}
