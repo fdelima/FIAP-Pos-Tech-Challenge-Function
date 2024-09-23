718f222b71db511cccad5feec0af76696d6cfaa5
Fernando Lima <fernando@umavida.com>
2024-09-23 18:48:51 -0300

    PreserveNewest

--- Src/Azure/TokenValidationFunctionApp/TokenValidationFunction.cs
+++ Src/Azure/TokenValidationFunctionApp/TokenValidationFunction.cs
@@ -8,7 +8,6 @@ using Microsoft.IdentityModel.Protocols.OpenIdConnect;
 using Microsoft.IdentityModel.Tokens;
 using System;
 using System.IdentityModel.Tokens.Jwt;
-using System.Linq;
 using System.Security.Claims;
 using System.Text;
 using System.Threading.Tasks;
