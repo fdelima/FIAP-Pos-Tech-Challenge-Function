a7448633993f2279e55241178aed80b91261ef4f
Fernando Lima <fernando@umavida.com>
2024-09-23 17:58:46 -0300

    Teste in net6.0

--- Src/Azure/TokenValidationFunctionApp/SecurityConfig.cs
+++ Src/Azure/TokenValidationFunctionApp/SecurityConfig.cs
@@ -4,8 +4,8 @@ namespace TokenValidationFunctionApp
 {
     internal static class SecurityConfig
     {
-        internal static string[] ValidIssuers = [Environment.GetEnvironmentVariable("ValidIssuer")];
-        internal static string[] ValidAudiences = [Environment.GetEnvironmentVariable("ValidIssuer")];
+        internal static string[] ValidIssuers = new string[1] { Environment.GetEnvironmentVariable("ValidIssuer") };
+        internal static string[] ValidAudiences = new string[1] { Environment.GetEnvironmentVariable("ValidIssuer")};
         internal static string OpenIdConnectConfigurationUrl = $"{Environment.GetEnvironmentVariable("OpenIdConnectConfigurationUrl")}";
     }
 }
